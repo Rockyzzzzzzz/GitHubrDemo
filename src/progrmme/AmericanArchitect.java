1cacd6b6872f95ebd42854c984c1ca7aff0261e2
rakesh <rakesh@gmail.com>
2025-05-25 02:35:19 +0530

    american guy commit

--- src/progrmme/AmericanArchitect.java
+++ src/progrmme/AmericanArchitect.java
@@ -7,6 +7,9 @@ public class AmericanArchitect {
 		System.out.println("i done");
 		System.out.println("i done now ");
 		System.out.println("i done now");
+		
+		System.out.println("changes ");
+		System.out.println("changes");
 	}
 
 }
